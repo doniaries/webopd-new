6f4346020f898a13750769cbda000b0cc5d77daf
doni <doniaries@gmail.com>
2025-12-26 23:09:16 +0700

    stat widget

--- app/Filament/Widgets/StatsOverviewWidget.php
+++ app/Filament/Widgets/StatsOverviewWidget.php
@@ -2,6 +2,11 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\Post;
+use App\Models\Pengumuman;
+use App\Models\AgendaKegiatan;
+use App\Models\Dokumen;
+use App\Models\User;
 use Filament\Widgets\StatsOverviewWidget as BaseStatsOverviewWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
@@ -10,7 +15,35 @@ class StatsOverviewWidget extends BaseStatsOverviewWidget
     protected function getStats(): array
     {
         return [
-            //
+            Stat::make('Total Berita', Post::count())
+                ->description('Jumlah berita yang dipublikasikan')
+                ->descriptionIcon('heroicon-m-newspaper')
+                ->color('success')
+                ->chart([7, 3, 4, 5, 6, 3, 5, 3]),
+
+            Stat::make('Total Pengumuman', Pengumuman::count())
+                ->description('Jumlah pengumuman')
+                ->descriptionIcon('heroicon-m-megaphone')
+                ->color('warning')
+                ->chart([3, 5, 4, 6, 7, 5, 6, 8]),
+
+            Stat::make('Total Agenda', AgendaKegiatan::count())
+                ->description('Jumlah agenda kegiatan')
+                ->descriptionIcon('heroicon-m-calendar-days')
+                ->color('info')
+                ->chart([5, 4, 6, 5, 7, 6, 8, 7]),
+
+            Stat::make('Total Dokumen', Dokumen::count())
+                ->description('Jumlah dokumen tersedia')
+                ->descriptionIcon('heroicon-m-document-text')
+                ->color('primary')
+                ->chart([4, 6, 5, 7, 6, 8, 7, 9]),
+
+            Stat::make('Total Pengguna', User::count())
+                ->description('Jumlah pengguna terdaftar')
+                ->descriptionIcon('heroicon-m-users')
+                ->color('danger')
+                ->chart([2, 3, 4, 3, 5, 4, 6, 5]),
         ];
     }
 }
