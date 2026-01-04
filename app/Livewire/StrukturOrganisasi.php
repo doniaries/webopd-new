233b1c502e0c2f65c08d9ded2ca665a4a1968d20
doni <doniaries@gmail.com>
2026-01-05 01:07:41 +0700

    ssss

--- app/Livewire/StrukturOrganisasi.php
+++ app/Livewire/StrukturOrganisasi.php
@@ -7,41 +7,17 @@ use Livewire\Component;
 use Livewire\Attributes\Layout;
 use Livewire\Attributes\Lazy;
 
-#[Lazy]
 class StrukturOrganisasi extends Component
 {
     #[Layout('components.layouts.app')]
     public function render()
     {
-        $kepalaDinas = StrukturModel::where('name', 'Kepala Dinas')->first();
-        $sekretariat = StrukturModel::where('name', 'Sekretariat')->first();
-        $subBagianTataUsaha = StrukturModel::where('name', 'Sub Bagian Tata Usaha')->first();
-
-        $bidang = StrukturModel::whereNotIn('name', [
-            'Kepala Dinas',
-            'Sekretariat',
-            'Sub Bagian Tata Usaha',
-            'Unit Pelayanan Teknis Daerah (UPTD)',
-            'Sub Bagian Tata Usaha UPTD'
-        ])->get();
-
-        $uptd = StrukturModel::where('name', 'Unit Pelayanan Teknis Daerah (UPTD)')->first();
-        $subBagianUptd = StrukturModel::where('name', 'Sub Bagian Tata Usaha UPTD')->first();
+        $strukturOrganisasi = StrukturModel::all();
 
         return view('livewire.struktur-organisasi', [
-            'kepalaDinas' => $kepalaDinas,
-            'sekretariat' => $sekretariat,
-            'subBagianTataUsaha' => $subBagianTataUsaha,
-            'bidang' => $bidang,
-            'uptd' => $uptd,
-            'subBagianUptd' => $subBagianUptd,
+            'strukturOrganisasi' => $strukturOrganisasi,
             'pageTitle' => 'Struktur Organisasi',
             'pageDescription' => 'Struktur Organisasi Dinas Komunikasi dan Informatika'
         ]);
-    }
-
-    public function placeholder()
-    {
-        return view('livewire.placeholders.skeleton-detail');
     }
 }
